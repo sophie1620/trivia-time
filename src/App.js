57c07f852f5ab8ffb459b5135157acb7bcf3efb3
sophie <sophie1620@gmail.com>
2022-03-21 15:13:47 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ avatar, setAvatar ] = useState('')
+
+  // make axios call to Trivia API
+  useEffect( () => {
+    axios({
+      url: 'https://opentdb.com/api.php?amount=20', 
+      params: {
+        category: 14
+      }
+    }).then( (apiData) => {
+      console.log('Trivia Data', apiData.data.results);
+    })
+  }, [])
+
+
+  // ////// .map() through and push the correct answer and incorrect answer into a new array and then this array should be shuffled
+
+  // make axios call to DiceBear API
+  useEffect( () => {
+    axios({
+      url: 'https://avatars.dicebear.com/api/bottts/michelle.svg'
+    }).then( (data) => {
+      console.log('DiceBear Data:', data)
+      setAvatar(data.request.responseURL)
+    })
+  }, [])
+
+  console.log(avatar)
+
+  // /////// save player's names into a variable and then do a .map or something to put each name into its 
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <img src={`data:image/svg+xml;utf8,${avatar}`} /> */}
+
+
+      <div>
+        <img src={avatar} alt="" />
+      </div>
+
     </div>
   );
 }
