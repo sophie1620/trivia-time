4ce5ca35c4ff8d8163a6100bd6cdf305e95fe278
michi <michellewong1998@gmail.com>
2022-03-25 01:14:47 -0400

    fix radio button issue

--- src/components/PlayerQuestions.js
+++ src/components/PlayerQuestions.js
@@ -1,12 +1,47 @@
 import { useState } from 'react'
 
 function PlayerQuestions(props) {
-
+    const [active, setActive] = useState(0);
     function handleSelect(event) {
 
-        if (event.target.value === props.rightAnswer) {
-            props.increaseScore()
-        }
+        // if (event.target.value === props.rightAnswer) {
+        //     props.increaseScore()
+        // }
+    }
+
+    function normalizeQuestion(question) {
+        return question.replace(/&[#039]*;/g, "'").replace(/&[amp]*;/g, '&').replace(/&[quot]*;/g, '"')
+            .replace(/&[rsquo]*;/g, '’')
+            .replace(/&[lsquo]*;/g, '‘')
+            .replace(/&[ldquo]*;/g, '“')
+            .replace(/&[rdquo]*;/g, '”')
+            .replace(/&[apos]*;/gd, "'")
+            .replace(/&[hellip]*;/g, "…")
+            .replace(/&[percnt]*;/g, '%')
+            .replace(/&[divide]*;/g, '÷')
+            .replace(/&[div]*;/g, '÷')
+            .replace(/&[lt]*;/g, '<')
+            .replace(/&[gt]*;/g, '>')
+            .replace(/&[sup2]*;/g, '²')
+            .replace(/&[deg]*;/g, '°')
+            .replace(/&[aacute]*;/g, 'á')
+            .replace(/&[aAring]*;/g, 'Å')
+            .replace(/&[eacute]*;/g, 'é')
+            .replace(/&[iacute]*;/g, 'í')
+            .replace(/&[ntilde]*;/g, 'ñ')
+            .replace(/&[oacirc]*;/g, 'ô')
+            .replace(/&[oacute]*;/g, 'ó')
+            .replace(/&[uacute]*;/g, 'ú')
+            .replace(/&[auml]*;/g, 'ä')
+            .replace(/&[euml]*;/g, 'ë')
+            .replace(/&[iuml]*;/g, 'ï')
+            .replace(/&[ouml]*;/g, 'ö')
+            .replace(/&[uuml]*;/g, 'ü')
+            .replace(/&[yuml]*;/g, 'ÿ')
+            .replace(/&[uuml]*;/g, 'ü')
+            .replace(/&[scaron]*;/g, 'š')
+            .replace(/&[epsilon]*;/g, 'ε')
+            .replace(/&[Phi]*;/g, 'φ')
     }
 
 
@@ -18,149 +53,22 @@ function PlayerQuestions(props) {
 
             <legend>{props.triviaQuestn}</legend>
 
-            <label htmlFor="ans1">{props.answers[0].replace(/&[#039]*;/g, "'").replace(/&[amp]*;/g, '&').replace(/&[quot]*;/g, '"')
-                .replace(/&[rsquo]*;/g, '’')
-                .replace(/&[lsquo]*;/g, '‘')
-                .replace(/&[ldquo]*;/g, '“')
-                .replace(/&[rdquo]*;/g, '”')
-                .replace(/&[apos]*;/gd, "'")
-                .replace(/&[hellip]*;/g, "…")
-                .replace(/&[percnt]*;/g, '%')
-                .replace(/&[divide]*;/g, '÷')
-                .replace(/&[div]*;/g, '÷')
-                .replace(/&[lt]*;/g, '<')
-                .replace(/&[gt]*;/g, '>')
-                .replace(/&[sup2]*;/g, '²')
-                .replace(/&[deg]*;/g, '°')
-                .replace(/&[aacute]*;/g, 'á')
-                .replace(/&[aAring]*;/g, 'Å')
-                .replace(/&[eacute]*;/g, 'é')
-                .replace(/&[iacute]*;/g, 'í')
-                .replace(/&[ntilde]*;/g, 'ñ')
-                .replace(/&[oacirc]*;/g, 'ô')
-                .replace(/&[oacute]*;/g, 'ó')
-                .replace(/&[uacute]*;/g, 'ú')
-                .replace(/&[auml]*;/g, 'ä')
-                .replace(/&[euml]*;/g, 'ë')
-                .replace(/&[iuml]*;/g, 'ï')
-                .replace(/&[ouml]*;/g, 'ö')
-                .replace(/&[uuml]*;/g, 'ü')
-                .replace(/&[yuml]*;/g, 'ÿ')
-                .replace(/&[uuml]*;/g, 'ü')
-                .replace(/&[scaron]*;/g, 'š')
-                .replace(/&[epsilon]*;/g, 'ε')
-                .replace(/&[Phi]*;/g, 'φ')}</label>
-            <input type="radio" name={props.triviaQuestn} id="ans1" value={props.answers[0]} onChange={handleSelect} />
+            <label htmlFor={`ans1-${props.triviaQuestn}}`}>{normalizeQuestion(props.answers[0])}</label>
+            <input type="radio" id={`ans1-${props.triviaQuestn}}`} value={props.answers[0]} onChange={handleSelect} checked={active == 0} onClick={() => setActive(0)} />
 
-            <label htmlFor="ans2">{props.answers[1].replace(/&[#039]*;/g, "'").replace(/&[amp]*;/g, '&').replace(/&[quot]*;/g, '"')
-                .replace(/&[rsquo]*;/g, '’')
-                .replace(/&[lsquo]*;/g, '‘')
-                .replace(/&[ldquo]*;/g, '“')
-                .replace(/&[rdquo]*;/g, '”')
-                .replace(/&[apos]*;/gd, "'")
-                .replace(/&[hellip]*;/g, "…")
-                .replace(/&[percnt]*;/g, '%')
-                .replace(/&[divide]*;/g, '÷')
-                .replace(/&[div]*;/g, '÷')
-                .replace(/&[lt]*;/g, '<')
-                .replace(/&[gt]*;/g, '>')
-                .replace(/&[sup2]*;/g, '²')
-                .replace(/&[deg]*;/g, '°')
-                .replace(/&[aacute]*;/g, 'á')
-                .replace(/&[aAring]*;/g, 'Å')
-                .replace(/&[eacute]*;/g, 'é')
-                .replace(/&[iacute]*;/g, 'í')
-                .replace(/&[ntilde]*;/g, 'ñ')
-                .replace(/&[oacirc]*;/g, 'ô')
-                .replace(/&[oacute]*;/g, 'ó')
-                .replace(/&[uacute]*;/g, 'ú')
-                .replace(/&[auml]*;/g, 'ä')
-                .replace(/&[euml]*;/g, 'ë')
-                .replace(/&[iuml]*;/g, 'ï')
-                .replace(/&[ouml]*;/g, 'ö')
-                .replace(/&[uuml]*;/g, 'ü')
-                .replace(/&[yuml]*;/g, 'ÿ')
-                .replace(/&[uuml]*;/g, 'ü')
-                .replace(/&[scaron]*;/g, 'š')
-                .replace(/&[epsilon]*;/g, 'ε')
-                .replace(/&[Phi]*;/g, 'φ')}</label>
-            <input type="radio" name={props.triviaQuestn} id="ans2" value={props.answers[1]} onChange={handleSelect} />
+            <label htmlFor={`ans2-${props.triviaQuestn}}`}>{normalizeQuestion(props.answers[1])}</label>
+            <input type="radio" id={`ans2-${props.triviaQuestn}}`} value={props.answers[1]} onChange={handleSelect} checked={active == 1} onClick={() => setActive(1)} />
 
-            <label htmlFor="ans3">{props.answers[2].replace(/&[#039]*;/g, "'").replace(/&[amp]*;/g, '&').replace(/&[quot]*;/g, '"')
-                .replace(/&[rsquo]*;/g, '’')
-                .replace(/&[lsquo]*;/g, '‘')
-                .replace(/&[ldquo]*;/g, '“')
-                .replace(/&[rdquo]*;/g, '”')
-                .replace(/&[apos]*;/gd, "'")
-                .replace(/&[hellip]*;/g, "…")
-                .replace(/&[percnt]*;/g, '%')
-                .replace(/&[divide]*;/g, '÷')
-                .replace(/&[div]*;/g, '÷')
-                .replace(/&[lt]*;/g, '<')
-                .replace(/&[gt]*;/g, '>')
-                .replace(/&[sup2]*;/g, '²')
-                .replace(/&[deg]*;/g, '°')
-                .replace(/&[aacute]*;/g, 'á')
-                .replace(/&[aAring]*;/g, 'Å')
-                .replace(/&[eacute]*;/g, 'é')
-                .replace(/&[iacute]*;/g, 'í')
-                .replace(/&[ntilde]*;/g, 'ñ')
-                .replace(/&[oacirc]*;/g, 'ô')
-                .replace(/&[oacute]*;/g, 'ó')
-                .replace(/&[uacute]*;/g, 'ú')
-                .replace(/&[auml]*;/g, 'ä')
-                .replace(/&[euml]*;/g, 'ë')
-                .replace(/&[iuml]*;/g, 'ï')
-                .replace(/&[ouml]*;/g, 'ö')
-                .replace(/&[uuml]*;/g, 'ü')
-                .replace(/&[yuml]*;/g, 'ÿ')
-                .replace(/&[uuml]*;/g, 'ü')
-                .replace(/&[scaron]*;/g, 'š')
-                .replace(/&[epsilon]*;/g, 'ε')
-                .replace(/&[Phi]*;/g, 'φ')}</label>
-            <input type="radio" name={props.triviaQuestn} id="ans3" value={props.answers[2]} onChange={handleSelect} />
+            <label htmlFor={`ans3-${props.triviaQuestn}}`}>{normalizeQuestion(props.answers[2])}</label>
+            <input type="radio" id={`ans3-${props.triviaQuestn}}`} value={props.answers[2]} onChange={handleSelect} checked={active == 2} onClick={() => setActive(2)} />
 
-            <label htmlFor="ans4">{props.answers[3].replace(/&[#039]*;/g, "'").replace(/&[amp]*;/g, '&').replace(/&[quot]*;/g, '"')
-                .replace(/&[rsquo]*;/g, '’')
-                .replace(/&[lsquo]*;/g, '‘')
-                .replace(/&[ldquo]*;/g, '“')
-                .replace(/&[rdquo]*;/g, '”')
-                .replace(/&[apos]*;/gd, "'")
-                .replace(/&[hellip]*;/g, "…")
-                .replace(/&[percnt]*;/g, '%')
-                .replace(/&[divide]*;/g, '÷')
-                .replace(/&[div]*;/g, '÷')
-                .replace(/&[lt]*;/g, '<')
-                .replace(/&[gt]*;/g, '>')
-                .replace(/&[sup2]*;/g, '²')
-                .replace(/&[deg]*;/g, '°')
-                .replace(/&[aacute]*;/g, 'á')
-                .replace(/&[aAring]*;/g, 'Å')
-                .replace(/&[eacute]*;/g, 'é')
-                .replace(/&[iacute]*;/g, 'í')
-                .replace(/&[ntilde]*;/g, 'ñ')
-                .replace(/&[oacirc]*;/g, 'ô')
-                .replace(/&[oacute]*;/g, 'ó')
-                .replace(/&[uacute]*;/g, 'ú')
-                .replace(/&[auml]*;/g, 'ä')
-                .replace(/&[euml]*;/g, 'ë')
-                .replace(/&[iuml]*;/g, 'ï')
-                .replace(/&[ouml]*;/g, 'ö')
-                .replace(/&[uuml]*;/g, 'ü')
-                .replace(/&[yuml]*;/g, 'ÿ')
-                .replace(/&[uuml]*;/g, 'ü')
-                .replace(/&[scaron]*;/g, 'š')
-                .replace(/&[epsilon]*;/g, 'ε')
-                .replace(/&[Phi]*;/g, 'φ')}</label>
-            <input type="radio" name={props.triviaQuestn} id="ans4" value={props.answers[3]} onChange={handleSelect} />
+            <label htmlFor={`ans4-${props.triviaQuestn}}`}>{normalizeQuestion(props.answers[3])}</label>
+            <input type="radio" id={`ans4-${props.triviaQuestn}}`} value={props.answers[3]} onChange={handleSelect} checked={active == 3} onClick={() => setActive(3)} />
 
 
         </fieldset>
 
     )
-
-
-
 }
 
 
