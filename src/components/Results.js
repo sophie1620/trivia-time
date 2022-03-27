3ae58ac263c05adaceb5790ec11decda63be8887
seannastewart <seannastew@gmail.com>
2022-03-27 13:05:00 -0400

    worked on results page

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,4 @@
 function Results(props) {
-    console.log(props.finalScores)
-    console.log(props)
 
 
 
@@ -8,9 +6,10 @@ function Results(props) {
         props.finalScores.map((info) => {
             return (
                 <div>
-                    <p>Player: {info.name}</p>
+                    <p>{info.name}: {info.points}</p>
+
                     <img src={info.pic} alt="Player avatar" />
-                    <p>Score {info.points}</p>
+
                 </div>
             )
         }));
