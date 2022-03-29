bd20e048056485799119e81b634ba8e27b7ba97e
sylvia <sylviaraposo@hotmail.ca>
2022-03-29 19:28:49 -0400

    added leaderboard styling

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import firebase from '../firebase';
 import { getDatabase, ref, onValue } from 'firebase/database';
 
@@ -30,26 +31,29 @@ function Leaderboard() {
                 <h3>Previous Winners</h3>
                 <ul className="winnersList">
                     {winners.map((winner) =>
-                    (<li key={Math.random()}>
-                        {
-                            winner.map((eachWinner) =>
-                            (
-                                <div className="listInfo">
-                                    <img
-                                        className="avatarPic"
-                                        src={eachWinner.pic}
-                                        alt="robot avatar"
-                                    />
-                                    <h4>{eachWinner.name}</h4>
-                                    <p>score: {eachWinner.points}</p>
-                                </div>
-                            )
-                            )
-                        }
-                    </li>
+                            <li key={Math.random()}>
+                                {
+                                    winner.map((eachWinner) =>
+                                    (
+                                        <div className="listInfo">
+                                            <img
+                                                className="avatarPic"
+                                                src={eachWinner.pic}
+                                                alt="robot avatar"
+                                            />
+                                            <h4>{eachWinner.name}</h4>
+                                            <p>score: {eachWinner.points}</p>
+                                        </div>
+                                    )
+                                    )
+                                }
+                            </li>
                     )
-                    )}
+                    }
                 </ul>
+                <Link to={"/"}>
+                    <button className="start finish">Play again</button>
+                </Link >
             </div>
         </section>
     )
